e212524b214ba24c5dea26222530a3da19a919a5
jim-nnamdi <jimsamuel50@gmail.com>
2021-10-23 19:56:39 +0100

    + add model file in functions cargo

--- structs/src/main.rs
+++ structs/src/main.rs
@@ -1,3 +1,26 @@
+struct User {
+    name: String,
+    email: String,
+    active: bool,
+    sign_in_count: u64,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let user1 = User {
+        name: String::from("samuel jim"),
+        email: String::from("jimsamuel50@gmail.com"),
+        active: true,
+        sign_in_count: 0,
+    };
+
+    build_user(user1.email, user1.name, user1.active, user1.sign_in_count);
+}
+
+fn build_user(email: String, name: String, is_active: bool, log_in_count: u64) -> User {
+    User {
+        email: email,
+        name: name,
+        active: is_active,
+        sign_in_count: log_in_count,
+    }
 }
---
+++ structs/src/model.rs
@@ -0,0 +1,8 @@
+pub struct User {
+   name: String,
+   email: String 
+}
+
+impl User {
+   pub fn add(&mut self, value: i32) -> 
+}
