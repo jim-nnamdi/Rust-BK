bb6ca36284b06a0c9cd8988be45893d2edb34f5d
jim-nnamdi <jimsamuel50@gmail.com>
2021-10-23 20:09:35 +0100

    + add diesel for ORM and run migrations

---
+++ rust_restapi/target/rls/debug/deps/rust_restapi-cb357005484c2ab1.d
@@ -0,0 +1,5 @@
+/Users/samueljim/projects/rust_restapi/target/rls/debug/deps/rust_restapi-cb357005484c2ab1.rmeta: src/main.rs
+
+/Users/samueljim/projects/rust_restapi/target/rls/debug/deps/rust_restapi-cb357005484c2ab1.d: src/main.rs
+
+src/main.rs:
---
+++ rust_restapi/target/rls/debug/deps/rust_restapi-e650efea3b311050.d
@@ -0,0 +1,5 @@
+/Users/samueljim/projects/rust_restapi/target/rls/debug/deps/rust_restapi-e650efea3b311050.rmeta: src/main.rs
+
+/Users/samueljim/projects/rust_restapi/target/rls/debug/deps/rust_restapi-e650efea3b311050.d: src/main.rs
+
+src/main.rs:
