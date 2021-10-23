dc71a881761d43aa5c33c07dd43b78f3ebb4b002
jim-nnamdi <jimsamuel50@gmail.com>
2021-10-23 23:45:08 +0100

    + initialise struct for config

--- rust_restapi/target/rls/debug/deps/rust_restapi-0fa1f5dfbedf29bb.d
+++ rust_restapi/target/rls/debug/deps/rust_restapi-0fa1f5dfbedf29bb.d
@@ -1,6 +1,7 @@
-/Users/samueljim/projects/rust_restapi/target/rls/debug/deps/rust_restapi-0fa1f5dfbedf29bb.rmeta: src/main.rs src/schema.rs
+/Users/samueljim/projects/rust_restapi/target/rls/debug/deps/rust_restapi-0fa1f5dfbedf29bb.rmeta: src/main.rs src/config.rs src/schema.rs
 
-/Users/samueljim/projects/rust_restapi/target/rls/debug/deps/rust_restapi-0fa1f5dfbedf29bb.d: src/main.rs src/schema.rs
+/Users/samueljim/projects/rust_restapi/target/rls/debug/deps/rust_restapi-0fa1f5dfbedf29bb.d: src/main.rs src/config.rs src/schema.rs
 
 src/main.rs:
+src/config.rs:
 src/schema.rs:
